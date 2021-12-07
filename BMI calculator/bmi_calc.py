c6e97614ee3f717f7d831dc26888577685f7c503
Red-is-Ded <85179501+Red-is-Ded@users.noreply.github.com>
2021-12-07 07:47:59 +0100

    bmi calc

---
+++ BMI calculator/bmi_calc.py
@@ -0,0 +1,12 @@
+bmi = weight / (height * height)
+bmi2 = round(bmi)
+if bmi < 18.5:
+    print(f"Your BMI is {bmi2}, you are underweight.")
+elif bmi2 < 25:
+     print(f"Your BMI is {bmi2}, you have a normal weight.")
+elif bmi2 < 30:
+     print(f"Your BMI is {bmi2}, you are slightly overweight.")
+elif bmi2 < 35:
+     print(f"Your BMI is {bmi2}, you are obese.")
+else:
+     print(f"Your BMI is {bmi2}, you are clinically obese.")
